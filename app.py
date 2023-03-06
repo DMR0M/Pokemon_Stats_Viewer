13460b04aeb2be5434a0cac92aa8dd37f579072c
Rommel <rommeldm87@gmail.com>
2023-03-06 20:04:08 +0800

    fixed info layout

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from itertools import chain
 import template
 # from PIL import Image
 
@@ -11,11 +12,13 @@ CSV_PATH = Path.cwd() / 'csv'
 # Gen 1
 Gen_1 = CSV_PATH / 'pkmn_dataset_gen1.csv'
 Gen_2 = CSV_PATH / 'pkmn_dataset_gen2.csv'
+Gen_3 = CSV_PATH / 'pkmn_dataset_gen3.csv'
 
 # List of Pokemon Data Sets
 pkmn_data_sets = CSV_PATH.iterdir()
 gen_df1 = pd.read_csv(Gen_1)
 gen_df2 = pd.read_csv(Gen_2)
+gen_df3 = pd.read_csv(Gen_3)
 
 # Description Column
 desc_columns = list(gen_df1.keys()[:5])
@@ -46,79 +49,60 @@ st.header('Generation 1 Pokemon')
 st.markdown('***')
 col_1, col_2, col_3, col_4 = st.columns([1, 1, 1, 1])
 
+
 with col_1:
 	for i, row in gen_df1[:38].iterrows():
+		st.subheader('Pokedex Number: ')
 		st.info(row['Pokedex_Number'][2:])
 		st.header(row['Name'])
-		st.image(f"gen1/{i + 1}.png", width=160)
+		st.image(f"gen1/{i + 1}.png", width=120)
+
+		st.subheader('Type: ')
 		st.subheader(row['Type'])
+
+		st.subheader('Ability: ')
 		st.info(row['Ability'])
 		st.markdown('***')
 
 with col_2:
 	for i, row in gen_df1[38:76].iterrows():
+		st.subheader('Pokedex Number: ')
 		st.info(row['Pokedex_Number'][2:])
 		st.header(row['Name'])
-		st.image(f"gen1/{i + 1}.png", width=160)
+		st.image(f"gen1/{i + 1}.png", width=120)
+
+		st.subheader('Type: ')
 		st.subheader(row['Type'])
+
+		st.subheader('Ability: ')
 		st.info(row['Ability'])
 		st.markdown('***')
 
 with col_3:
 	for i, row in gen_df1[76:114].iterrows():
+		st.subheader('Pokedex Number: ')
 		st.info(row['Pokedex_Number'][2:])
 		st.header(row['Name'])
-		st.image(f"gen1/{i + 1}.png", width=160)
+		st.image(f"gen1/{i + 1}.png", width=120)
+
+		st.subheader('Type: ')
 		st.subheader(row['Type'])
+
+		st.subheader('Ability: ')
 		st.info(row['Ability'])
 		st.markdown('***')
 
 with col_4:
 	for i, row in gen_df1[114:].iterrows():
+		st.subheader('Pokedex Number: ')
 		st.info(row['Pokedex_Number'][2:])
 		st.header(row['Name'])
-		st.image(f"gen1/{i + 1}.png", width=160)
+		st.image(f"gen1/{i + 1}.png", width=120)
+
+		st.subheader('Type: ')
 		st.subheader(row['Type'])
+
+		st.subheader('Ability: ')
 		st.info(row['Ability'])
 		st.markdown('***')
 
-
-st.header('Generation 2 Pokemon')
-st.markdown('***')
-col_5, col_6, col_7, col_8 = st.columns([1, 1, 1, 1])
-
-with col_5:
-	for i, row in gen_df2[:25].iterrows():
-		st.info(row['Pokedex_Number'][2:])
-		st.header(row['Name'])
-		st.image(f"gen2/{i + 1}.png", width=160)
-		st.subheader(row['Type'])
-		st.info(row['Ability'])
-		st.markdown('***')
-
-with col_6:
-	for i, row in gen_df2[25:50].iterrows():
-		st.info(row['Pokedex_Number'][2:])
-		st.header(row['Name'])
-		st.image(f"gen2/{i + 1}.png", width=160)
-		st.subheader(row['Type'])
-		st.info(row['Ability'])
-		st.markdown('***')
-
-with col_7:
-	for i, row in gen_df2[50:75].iterrows():
-		st.info(row['Pokedex_Number'][2:])
-		st.header(row['Name'])
-		st.image(f"gen2/{i + 1}.png", width=160)
-		st.subheader(row['Type'])
-		st.info(row['Ability'])
-		st.markdown('***')
-
-with col_8:
-	for i, row in gen_df2[100:].iterrows():
-		st.info(row['Pokedex_Number'][2:])
-		st.header(row['Name'])
-		st.image(f"gen2/{i + 1}.png", width=160)
-		st.subheader(row['Type'])
-		st.info(row['Ability'])
-		st.markdown('***')
